012562006fe584e8335097c836904abd0fc39752
thetrueseros <leugimdzn@hotmail.com>
2026-04-28 14:55:45 -0500

    feat: configuración de la estructura base de la API. Se habilitaron controladores en ASP.NET Core. Se creó el proyecto Via360.Shared.

---
+++ Via360-Backend/Via360.API/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Via360.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("API funcionando con controladores.");
+    }
+}
