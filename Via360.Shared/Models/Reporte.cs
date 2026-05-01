3d342f4d5e17c2e3dc93090298ce3ff18f661d6b
thetrueseros <leugimdzn@hotmail.com>
2026-05-01 13:15:01 -0500

    Creación de los atributos de la clase Reporte.

--- Via360.Shared/Models/Reporte.cs
+++ Via360.Shared/Models/Reporte.cs
@@ -6,9 +6,23 @@ namespace Via360.Shared.Models
 {
     public class Reporte
     {
-        public string Id { get; set; } = string.Empty;
-        public string Descripcion { get; set; } = string.Empty;
-        public DateTime Fecha { get; set; } = DateTime.Now;
-        public string Estado { get; set; } = "Pendiente";
+        private string idReporte { get; set; } = "string.Empty";
+        private DateTime fecha { get; set; } = DateTime.Now;
+        private enum TipoIncidente
+        {
+            Accidente,
+            SemáforoAveriado,
+            Bache,
+            ObstrucciónVial,
+            ObraEnLaVía,
+            Otro
+        }
+        private string descripcion { get; set; } = "";
+        private enum Estado
+        {
+            Pendiente,
+            EnProceso,
+            Resuelto
+        }
     }
 }
