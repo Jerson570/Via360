c831310eda15738055bfb315498c7e53ef979eef
thetrueseros <leugimdzn@hotmail.com>
2026-05-02 13:56:17 -0500

    Se creó la clase abstracta Usuario.

---
+++ Via360.Shared/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Via360.Shared.Models
+{
+    public abstract class Usuario
+    {
+        public string IdUsuario { get; private set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string HashContraseña { get; set; }
+        public Usuario() { }
+        public Usuario(string nombre, string email, string ContraseñaPlana)
+        {
+            this.IdUsuario = Guid.NewGuid().ToString();
+            this.Nombre = nombre;
+            this.Email = email;
+        }
+    }
+}
