99d8a76e58f29bfd168ee6c98b5975f42671aab7
thetrueseros <leugimdzn@hotmail.com>
2026-05-03 17:34:42 -0500

    Implementación interfaz IAuthService para Autenticación

---
+++ Via360-App/Services/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Via360.App.Services
+{
+    public interface IAuthService
+    {
+        Task<string> RegistroAsync(string email, string password);
+        Task<string> LoginAsync(string email, string password);
+        void Logout();
+        bool IsLoggedIn();
+    }
+}
