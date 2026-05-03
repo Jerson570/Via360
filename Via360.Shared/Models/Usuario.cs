00044a877e74f25e44d235a8918b84844c50811d
thetrueseros <leugimdzn@hotmail.com>
2026-05-02 22:53:52 -0500

    Se eliminaron las clases Autoridad y Ciudadano y se convirtió Usuario a No Abstracta. Se le añadieron los campos extra de Autoridad como string nullable

--- Via360.Shared/Models/Usuario.cs
+++ Via360.Shared/Models/Usuario.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.Text;
 namespace Via360.Shared.Models
 {
-    public abstract class Usuario
+    public class Usuario
     {
-        public string IdUsuario { get; set; }
+        public string IdUsuario { get; set; } // UID de firebase
         public string Nombre { get; set; }
         public string Email { get; set; }
+        public string Rol { get; set; } // "Ciudadano" o "Autoridad"
         public DateTime FechaRegistro {  get; set; }
+
+        // campos exclusivos de autoridades (NULL para ciudadanos)
+        public string? Cargo { get; set; }
+        public string? Entidad { get; set; }
+
         public Usuario() { }
-        public Usuario(string id, string nombre, string email)
+        public Usuario(string id, string nombre, string email, string rol = "Ciudadano")
         {
             this.IdUsuario = id;
             this.Nombre = nombre;
             this.Email = email;
-            this.FechaRegistro = DateTime.Now;
+            this.FechaRegistro = DateTime.UtcNow;
+            this.Rol = rol;
         }
     }
 }
