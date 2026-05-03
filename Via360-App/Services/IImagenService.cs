4d216f75e6fdcdf76d5759b6fb83c360efeb6bb4
thetrueseros <leugimdzn@hotmail.com>
2026-05-03 17:32:30 -0500

    Interfaz IImagenService

---
+++ Via360-App/Services/IImagenService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Via360.App.Services
+{
+    public interface IImagenService
+    {
+        Task<string> SubirImagenAsync(FileResult foto);
+    }
+}
