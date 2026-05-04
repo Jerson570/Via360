a3277f770c3f0d547657f048f3311944c2f9b844
thetrueseros <leugimdzn@hotmail.com>
2026-05-03 21:15:09 -0500

    Se incluyó el RegisterRoute de la pantalla principal en el AppShell y el AddTransient en el MauiProgram.

--- Via360-App/AppShell.xaml.cs
+++ Via360-App/AppShell.xaml.cs
@@ -8,6 +8,7 @@ namespace Via360.App
         {
             InitializeComponent();
 
+            Routing.RegisterRoute(nameof(PantallaPrincipal), typeof(PantallaPrincipal));
             Routing.RegisterRoute(nameof(RegistroPage), typeof(RegistroPage));
             Routing.RegisterRoute(nameof(RecuperarPage), typeof(RecuperarPage));
             Routing.RegisterRoute(nameof(PerfilPage), typeof(PerfilPage));
