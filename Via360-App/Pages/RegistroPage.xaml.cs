ca5cf8b943bc1af91c1189cadc432e7e133d7cbf
thetrueseros <leugimdzn@hotmail.com>
2026-05-03 23:49:25 -0500

    Se reemplazó la lógica antigua de Realtime Database por Mvvm en RegistroPage.xaml.cs.

--- Via360-App/Pages/RegistroPage.xaml.cs
+++ Via360-App/Pages/RegistroPage.xaml.cs
@@ -1,58 +1,12 @@
-using Firebase.Database;
-using Firebase.Database.Query;
+using Via360.App.ViewModels;
+
 namespace Via360.App;
 
 public partial class RegistroPage : ContentPage
 {
-    FirebaseClient client = new FirebaseClient("https://via360-app-default-rtdb.firebaseio.com/");
-    public RegistroPage()
-	{
-        InitializeComponent();
-	}
-
-    private async void OnFinalizarRegistroClicked(object sender, EventArgs e)
+    public RegistroPage(RegistroViewModel viewModel)
     {
-        // 1. Verificación básica
-        if (string.IsNullOrWhiteSpace(TxtNombre.Text) ||
-            string.IsNullOrWhiteSpace(TxtEmail.Text) ||
-            string.IsNullOrWhiteSpace(TxtPassword.Text) ||
-            PickerRol.SelectedItem == null)
-        {
-            await this.DisplayAlertAsync("Atención", "Por favor completa todos los datos", "OK");
-            return;
-        }
-
-        try
-        {
-            // 2. Creamos el objeto con los datos
-            var nuevoUsuario = new Usuario
-            {
-                Nombre = TxtNombre.Text,
-                Correo = TxtEmail.Text,
-                Contrasena = TxtPassword.Text,
-                Rol = PickerRol.SelectedItem.ToString()
-            };
-
-            // Enviar a firebase
-            await client
-                .Child("Usuarios")
-                .PostAsync(nuevoUsuario);
-
-            await this.DisplayAlertAsync("Vía360", "Registro exitoso en la base de datos", "Aceptar");
-            await Shell.Current.GoToAsync("..");
-        }
-        catch (Exception ex)
-        {
-            await this.DisplayAlertAsync("Error", "Fallo al conectar con Firebase: " + ex.Message, "OK");
-        }
+        InitializeComponent();
+        BindingContext = viewModel;
     }
-}
-
-
-public class Usuario
-{
-    public string Nombre { get; set; }
-    public string Correo { get; set; }
-    public string Contrasena { get; set; }
-    public string Rol { get; set; }
 }
