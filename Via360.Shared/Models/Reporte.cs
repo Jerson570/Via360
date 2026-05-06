fdb13f44e7e429c606550264f8fe5bda99b016bb
thetrueseros <leugimdzn@hotmail.com>
2026-05-05 20:34:43 -0500

    Se removió el objeto Posición de Reportes

--- Via360.Shared/Models/Reporte.cs
+++ Via360.Shared/Models/Reporte.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Google.Cloud.Firestore;
 
 namespace Via360.Shared.Models
 {
     public class Reporte
     {
+        [FirestoreProperty("idReporte")]
         public string? IdReporte { get; set; }
+        [FirestoreProperty("idUsuario")]
         public string IdUsuario { get; set; }
+        [FirestoreProperty("fecha")]
         public DateTime Fecha { get; set; }
+        [FirestoreProperty("tipo")]
         public TipoIncidente Tipo { get; set; }
+        [FirestoreProperty("ubicacion")]
         public Ubicacion Ubicacion { get; set; }
+        [FirestoreProperty("descripcion")]
         public string Descripcion { get; set; }
+        [FirestoreProperty("estado")]
         public EstadoReporte Estado {  get; set; }
+        [FirestoreProperty("urlImagen")]
         public string? UrlImagen { get; set; }
-
-        public Ubicacion Posicion { get; set; } = new Ubicacion();
         public Reporte()
         {
 
