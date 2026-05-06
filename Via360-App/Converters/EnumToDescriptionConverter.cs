0a4e7f332b7861d77f1d8debfa6c626dfb4a8e4b
thetrueseros <leugimdzn@hotmail.com>
2026-05-05 22:25:17 -0500

    se creó el conveertidor EnumToDescriptionConverter para convertir el enum a un string con espacios.

---
+++ Via360-App/Converters/EnumToDescriptionConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+
+namespace Via360.App.Converters;
+
+public class EnumToDescriptionConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Enum enumValue)
+        {
+            var field = enumValue.GetType().GetField(enumValue.ToString());
+            var attribute = field?.GetCustomAttribute<DescriptionAttribute>();
+
+            // Si tiene [Description], úsalo. Si no, usa el nombre del Enum.
+            return attribute?.Description ?? enumValue.ToString();
+        }
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
