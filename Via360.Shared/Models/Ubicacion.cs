de2a705357b6161b147163c419a59c35a2a711bd
thetrueseros <leugimdzn@hotmail.com>
2026-05-05 20:35:14 -0500

    Se incluyó la conversión de atributos para firebase en Ubicacion.

--- Via360.Shared/Models/Ubicacion.cs
+++ Via360.Shared/Models/Ubicacion.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Google.Cloud.Firestore;
 
 namespace Via360.Shared.Models
 {
     public class Ubicacion
     {
+        [FirestoreProperty("latitud")]
         public double Latitud { get; set; }
+        [FirestoreProperty("longitud")]
         public double Longitud { get; set; }
+        [FirestoreProperty("direccionTexto")]
         public string? DireccionTexto { get; set; }
         public Ubicacion() { }
         public Ubicacion(double latitud, double longitud, string direccion = "")
