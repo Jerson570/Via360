12f9a12be8e1cb98a8c67c55cec264b210b45d03
thetrueseros <leugimdzn@hotmail.com>
2026-05-05 23:30:41 -0500

    Se añadió el builder.Services.AddTransient para la PantallaPrincipalAutoridad(); en MauiProgram y se registró la ruta de la misma en AppShell.xaml.cs

--- Via360-App/AppShell.xaml.cs
+++ Via360-App/AppShell.xaml.cs
@@ -9,6 +9,7 @@ namespace Via360.App
             InitializeComponent();
 
             Routing.RegisterRoute(nameof(PantallaPrincipal), typeof(PantallaPrincipal));
+            Routing.RegisterRoute(nameof(PantallaPrincipalAutoridad), typeof(PantallaPrincipalAutoridad));
             Routing.RegisterRoute(nameof(RegistroPage), typeof(RegistroPage));
             Routing.RegisterRoute(nameof(RecuperarPage), typeof(RecuperarPage));
             Routing.RegisterRoute(nameof(Via360.App.Pages.MisReportesPage), typeof(Via360.App.Pages.MisReportesPage));
--- Via360-App/MauiProgram.cs
+++ Via360-App/MauiProgram.cs
@@ -37,6 +37,7 @@ namespace Via360.App
 
             // registro de Páginas
             builder.Services.AddTransient<PantallaPrincipal>();
+            builder.Services.AddTransient<PantallaPrincipalAutoridad>();
             builder.Services.AddTransient<MainPage>();
             builder.Services.AddTransient<PerfilPage>();
             builder.Services.AddTransient<RegistroPage>();
