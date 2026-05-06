530904fe798810fd4a9df4146a5ba8cbdfd05512
thetrueseros <leugimdzn@hotmail.com>
2026-05-05 22:25:34 -0500

    Se implementó el converter InvertedBoolConverter

---
+++ Via360-App/Converters/InvertedBoolConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Via360.App.Converters
+{
+    public class InvertedBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is bool b ? !b : false;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
